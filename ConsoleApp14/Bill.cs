007025e0225bb3104db47b0e681d3944addb414c
Goran Vukojičić <goranvukojicic@gmail.com>
2022-06-27 11:57:12 +0200

    Add project files.

---
+++ ConsoleApp14/Bill.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Bill
+    {
+        int[] check = new int[] { 3, 10, 2, 9 };
+        int k = 1;
+        int b = 12;
+        public static string bonAppetite(int[] check,int k, int b)
+        {
+            int anns = 0;
+            string bon = "bon appetite";
+
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (i != k) anns += check[i];
+            }
+
+            if (anns == b)
+                return bon; 
+
+            else 
+            {
+                int razlika = anns - b;
+                return razlika.ToString();
+            }
+        }
+
+    }
+}
+
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] check = new int[] { 3, 10, 2, 9 };
+            int k = 1;
+            int b = 12;
+            
+            Console.WriteLine(Bill.bonAppetite(check,k,b));
+        }
+    }
+}
